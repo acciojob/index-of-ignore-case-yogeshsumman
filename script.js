90d9f29e238bf24cbea62f74693e18f24d5f6863
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-03-02 17:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+    s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+   if(s1.includes(s2)){
+    return s1.indexOf(s2);
+   }else{
+      return -1;
+   };
 }
 
 // Please do not change the code below
